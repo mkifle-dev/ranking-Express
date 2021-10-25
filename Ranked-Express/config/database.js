d609ef6a1e5863553db7b483f64aa539bafd842f
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-10-25 11:57:35 -0400

    update to include database info

---
+++ Ranked-Express/config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://auth-1:auth@cluster0.tdpfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'auth-1': 'auth'  
+};
+//const url = "mongodb+srv://user-one:BlueMoon@cluster0.5xllp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
